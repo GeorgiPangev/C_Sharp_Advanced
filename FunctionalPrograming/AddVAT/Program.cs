23520dc6c8ca24cfca22ceb2290dc8eb6d40e92f
Georgi Pangev <PangevG@Gmail.com>
2021-05-03 18:50:30 +0300

    First Repo.

---
+++ FunctionalPrograming/AddVAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = x=> Console.WriteLine(string.Join(Environment.NewLine, x));
+            Func<double, double> vat = d => (d += d * 0.2);
+            Func<double, string> f = n => $"{n:F2}";
+            string[] prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(n => vat(n))
+                .Select(b=>f(b))
+                .ToArray();
+
+            print(prices);
+        }
+    }
+}
