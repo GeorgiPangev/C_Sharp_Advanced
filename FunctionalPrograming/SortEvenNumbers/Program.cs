8ed7fa1526e4131cc793f9713c5a1492d232f444
Georgi Pangev <PangevG@Gmail.com>
2021-05-08 13:52:13 +0300

    Sort Even Number Problem;

---
+++ FunctionalPrograming/SortEvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(n=>n%2==0)
+                .OrderBy(x => x)
+               .Select(n=> n.ToString())
+                //.Where(n =>char.IsDigit(char.Parse(n))
+                .ToArray();
+
+            Action <string[]> print = input => Console.WriteLine(string.Join(", ", input));
+            print(input);
+            
+            
+            //input = input.Select(x=>x.print(x));
+
+        }
+    }
+}
