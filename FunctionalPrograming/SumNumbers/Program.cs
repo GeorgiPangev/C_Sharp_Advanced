54d0422ae5ff30df0295c2f9525e2e8ad0b02f1b
Georgi Pangev <PangevG@Gmail.com>
2021-05-09 21:32:53 +0300

    Last problem added.

---
+++ FunctionalPrograming/SumNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+           Action<int> print = n => Console.WriteLine(n);
+            //Func<int[], string> print = p => p.ToString(); 
+           Check(input, print);
+        }
+
+        private static void Check(int[] input, Action<int> print)
+        {
+            print(input.Length);
+            print(input.Sum());
+        }
+    }
+}
