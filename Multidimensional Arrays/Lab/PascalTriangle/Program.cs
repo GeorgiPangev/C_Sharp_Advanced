2eb211b63f0e39504b3671b9c731db4c32339454
Georgi Pangev <PangevG@Gmail.com>
2021-05-11 19:59:41 +0300

    ++

---
+++ Multidimensional Arrays/Lab/PascalTriangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            ulong[][] piramid = new ulong[n][];
+
+            for (int row = 0; row < piramid.GetLength(0); row++)
+            {
+                piramid[row] = new ulong[row + 1];
+
+                for (int col = 0; col < piramid[row].Length; col++)
+                {
+                    if (col==0||col==piramid[row].Length-1)
+                    {
+                        piramid[row][col] = 1;
+                    }
+                    else 
+                    {
+                        piramid[row][col] = piramid[row - 1][col - 1] + piramid[row - 1][col];
+                    }
+                }
+            }
+
+            foreach (ulong[] row in piramid)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
