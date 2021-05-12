63367180b971f3b13d97d605f4d74410e1c017f2
Georgi Pangev <PangevG@Gmail.com>
2021-05-12 13:34:31 +0300

    ++

---
+++ Multidimensional Arrays/Lab/PrimaryDiagonal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            int[,] matrix = new int[x,x];
+
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+
+                }
+            }
+            for (int i = 0; i < x; i++)
+            {
+                sum += matrix[i, i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
