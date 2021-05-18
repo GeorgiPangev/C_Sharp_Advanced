31ef6e05f280cf83c35e8b9b72e5a8ad03cd37be
Georgi Pangev <PangevG@Gmail.com>
2021-05-18 21:59:59 +0300

    Last Problem

---
+++ Multidimensional Arrays/Lab/SumMatrixElement/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int[] dim = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int r = dim[0];
+            int c = dim[1];
+
+            int[,] matrix = new int[r, c];
+
+            
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+                for (int cow = 0; cow < matrix.GetLength(1); cow++)
+                {
+                    matrix[row, cow] = input[cow];
+                }
+
+               
+
+            }
+
+            int sum = 0;
+
+            foreach (int item in matrix)
+            {
+                sum += item;
+            }
+            //for (int row = 0; row < matrix.GetLength(0); row++)
+            //{
+            //    for (int col = 0; col < matrix.GetLength(1); col++)
+            //    {
+            //        sum += matrix[row, col];
+            //    }
+            //}
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
