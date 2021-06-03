fce36d8883ba1f2760885d7f91960ee69e582001
Georgi Pangev <PangevG@Gmail.com>
2021-06-03 23:57:12 +0300

    SimpleCalculator added.

---
+++ StacksAndQueusExs/Lab/3.SimpleCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            Stack<string> stack = new Stack<string>(input.Reverse());
+            while (stack.Count>=3)
+         
+            {
+                int x = int.Parse(stack.Pop());
+                string doThis = stack.Pop();
+                int y = int.Parse(stack.Pop());
+                if (doThis== "+")
+                {
+                    x += y;
+                    stack.Push(x.ToString());
+                }
+                else if (doThis=="-")
+                {
+                    stack.Push((x -= y).ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
