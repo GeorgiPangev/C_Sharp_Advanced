1cfd5fb93f5edca34687288588ec8b232717569c
Georgi Pangev <PangevG@Gmail.com>
2021-06-04 19:57:05 +0300

    MachingBracets problem added.

---
+++ StacksAndQueusExs/Lab/4.MatchingBrackets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (input[i] ==')' )
+                {
+                    stack.Push(i);
+                    int end = stack.Pop();
+                    int start = stack.Pop();
+                    string sub = input.Substring(start, (end+1 - start));
+                    Console.WriteLine(sub);
+                }
+            }
+        }
+    }
+}
