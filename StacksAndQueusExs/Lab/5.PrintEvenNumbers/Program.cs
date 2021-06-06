88be02a4bd10e4508a35cc924d33a554157dfca5
Georgi Pangev <PangevG@Gmail.com>
2021-06-06 18:28:24 +0300

    PrintEvenNumbers problem added.

---
+++ StacksAndQueusExs/Lab/5.PrintEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string inp = Console.ReadLine();
+            Queue<int> queue = new Queue<int>(new List<int>(Console.ReadLine().Split()
+                .Select(int.Parse).Where(x => x % 2 == 0)
+                .ToList()));
+            if (queue.Any())
+            {
+                //while (queue.Any())
+                //{
+                //    Console.Write($"{queue.Dequeue()}, ");
+                //}
+                Console.WriteLine(string.Join(", ",queue));
+            }
+        }
+    }
+}
