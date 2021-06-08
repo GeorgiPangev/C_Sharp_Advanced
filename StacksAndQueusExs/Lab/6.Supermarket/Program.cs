edd3d1a376422f88c29230e6eb5e756f0c9086cc
Georgi Pangev <PangevG@Gmail.com>
2021-06-08 18:27:14 +0300

    Supermarket problem added.

---
+++ StacksAndQueusExs/Lab/6.Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> customers = new Queue<string>();
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    int x = customers.Count;
+                    for (int i = 0; i < x; i++)
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                    }
+                }
+                else
+                {
+                    customers.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
