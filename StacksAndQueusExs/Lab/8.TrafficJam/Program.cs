199b915d2c9962785efeba600f0f7e9583293a21
Georgi Pangev <PangevG@Gmail.com>
2021-06-09 16:25:26 +0300

    TraficJam problem added;

---
+++ StacksAndQueusExs/Lab/8.TrafficJam/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int pasedCars = 0;
+            Queue<string> cars = new Queue<string>();
+            string input = Console.ReadLine();
+            while (input!="end")
+            {
+                if (input=="green")
+                {
+                    if (cars.Count>=n)
+                    {
+                        for (int i = 0; i < n; i++)
+
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            pasedCars++;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = cars.Count-1; i>=0; i--)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            pasedCars++;
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+                input = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"{pasedCars} cars passed the crossroads.");
+
+        }
+    }
+}
