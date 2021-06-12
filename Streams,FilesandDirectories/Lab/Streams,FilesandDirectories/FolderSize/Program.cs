23c070b386999e4e9bbf2ad81109b2ac04905a53
Georgi Pangev <PangevG@Gmail.com>
2021-06-12 20:35:19 +0300

    Streems, Files And Directoris Added

---
+++ Streams,FilesandDirectories/Lab/Streams,FilesandDirectories/FolderSize/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("../../../TestFolder");
+            long count = 0;
+            foreach (string file in files)
+            {
+                FileInfo info = new FileInfo(file);
+                count += info.Length;
+            }
+
+            count = count / 1024 / 1024;
+           File.WriteAllText("../../../ Output.txt",$"{count.ToString()}");
+            
+        }
+	
+
+	
+    }
+}
