c512f587096ed12eda5c66cf4e1376191154fe9d
Georgi Pangev <PangevG@Gmail.com>
2021-06-14 22:11:50 +0300

    ++

---
+++ Streams,FilesandDirectories/Lab/Streams,FilesandDirectories/LineFiles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace LineFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            using (StreamReader read = new StreamReader(@"../../../input.txt"))
+            {
+                string line = read.ReadLine();
+                using (StreamWriter write = new StreamWriter(@"../../../Output.txt"))
+                {
+                    while (line!=null)
+                    {
+                        counter++;
+                        write.WriteLine($"{counter}, {line}");
+                        line = read.ReadLine();
+                    }
+                    
+                }
+            }
+            
+
+        }
+    }
+}
