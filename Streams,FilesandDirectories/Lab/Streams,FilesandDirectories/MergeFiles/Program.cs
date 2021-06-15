df2d369b935640dabf03738856625633f1858d32
Georgi Pangev <PangevG@Gmail.com>
2021-06-15 23:21:28 +0300

    Merg files problem added.

---
+++ Streams,FilesandDirectories/Lab/Streams,FilesandDirectories/MergeFiles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader readOne = new StreamReader("../../../FileOne.txt");
+            StreamReader readTwo = new StreamReader("../../../FileTwo.txt");
+            StreamWriter write = new StreamWriter("../../../Output.txt");
+            using (readTwo)
+            {
+                using (readOne)
+                {
+                    string lineOne = readOne.ReadLine();
+                    string lineTwo = readTwo.ReadLine();
+                    
+                    while (readOne!=null||readTwo!=null)
+                    {
+                        write.WriteLine(lineOne);
+                        write.WriteLine(lineTwo);
+                        lineOne = readOne.ReadLine();
+                        lineTwo = readTwo.ReadLine();
+                    }
+
+                }
+            }
+        }
+    }
+}
