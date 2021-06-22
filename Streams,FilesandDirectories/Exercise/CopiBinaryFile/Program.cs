10ea00b84610a88a4a1bb96e69e6072f8d07583e
Georgi Pangev <PangevG@Gmail.com>
2021-06-22 18:45:37 +0300

    Copy Binari Files problem added plus files.

---
+++ Streams,FilesandDirectories/Exercise/CopiBinaryFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace CopiBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream reader = new FileStream("copyMe.png", FileMode.Open, FileAccess.Read);
+            using FileStream writer = new FileStream("../../../Output.png",  FileMode.Create, FileAccess.Write);
+
+            while (reader.Position< reader.Length)
+            {
+                byte[] bufer = new byte[4096];
+                reader.Read(bufer, 0, bufer.Length);
+                writer.Write(bufer);
+            }
+        }
+    }
+}
