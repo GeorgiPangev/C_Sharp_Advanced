932504df7c61268136f2b766f482d3c2d9067c0f
Georgi Pangev <PangevG@Gmail.com>
2021-06-25 00:10:47 +0300

    Line Numbers problem Added.

---
+++ Streams,FilesandDirectories/Exercise/LineNumbers/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //List<string> lines = new 
+            using (StreamReader reader = new StreamReader(@"../../../text.txt"))//FileMode.Open, FileAccess.Read))
+            {
+                using (StreamWriter write = new StreamWriter(@"../../../Output.txt"))//FileMode.Create))
+                {
+                    //string line = reader.ReadLine().ToString();
+                    int lineCount = 0;
+                    string line = reader.ReadLine();
+                    while (reader!=null)
+                    {
+                        if (reader==null)
+                        {
+                            break;
+                        }
+                        //string line = reader.ReadLine();
+                        int lethersCount = 0;
+                        int pointsCount = 0;
+                        lineCount++;
+                        for (int i = 0; i < line.Length ; i++)
+                        {
+                            if (char.IsLetter(line[i]))
+                            {
+                                lethersCount++;
+                            }
+                            else if (char.IsPunctuation(line[i]))
+                            {
+                                pointsCount++;
+                            }
+
+                        }
+                        string print = $"Line-{lineCount}: {line} ({lethersCount})({pointsCount})";
+                        write.WriteLine(print);
+                        if (reader.EndOfStream)
+                        {
+                            break;
+                        }
+                        line = reader.ReadLine();
+                       
+
+                    }
+                }
+            }
+
+        }
+    }
+}
